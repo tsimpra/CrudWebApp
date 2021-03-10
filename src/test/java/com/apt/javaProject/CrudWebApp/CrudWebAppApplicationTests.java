2bdbe556cff8c98b9c9dd84d3ff456ddf0eaf3f3
Vasilis Tsimpragakis <v.tsimpragakis@apt.gr>
2021-03-10 10:37:12 +0200

    project initialization

---
+++ src/test/java/com/apt/javaProject/CrudWebApp/CrudWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apt.javaProject.CrudWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
